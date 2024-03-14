9ed7bef5a865fec66745d163a2231e2b6d512217
Gianluca Donato <gianlucadonato2005@gmail.com>
2024-03-14 11:06:34 -0300

    crypto data fetching

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -13,3 +13,11 @@ export async function DolarOficial() {
 
   return DolarOficial;
 }
+
+export async function CryptoBTC() {
+  const CryptoValue = await fetch(
+    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,tether&vs_currencies=usd&include_last_updated_at=true"
+  ).then((res) => res.json());
+
+  return CryptoValue;
+}
